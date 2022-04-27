79f90acc1ca3dfa5e107b389596d835563bc3ea4
joe rizk <joerizk2@hotmail.com>
2022-04-28 00:12:16 +0300

    created all crud apis

--- storiad-laravel-challenge/app/Http/Controllers/BookController.php
+++ storiad-laravel-challenge/app/Http/Controllers/BookController.php
@@ -10,11 +10,14 @@ class BookController extends Controller
 {
     //
     public function createBook(Request $request){
-        // search for validation 
             $data = $request->all();
             $book = new Book;
+            $file= $request->file('book_image');
+            $filename= date('YmdHi').$file->getClientOriginalName();
+            $file-> move(public_path('/images'), $filename);
+
             $book->book_title = $data["book_title"];
-            $book->book_image = $data["book_image"];
+            $book->book_image = $filename;
             $book->description = $data["description"];
             $book->book_author = $data["book_author"];
             $book->book_genre = $data["book_genre"];
@@ -28,5 +31,41 @@ class BookController extends Controller
         return response()->json(["success" => true]);
     }
 
+    public function getBooks(){
+        $books = Book::all();
+
+        return response()->json($books);
+    }
+
+    public function editBook(Request $request) {
+        $book = Book::find($request['id']);
+        $data = $request->all();
+        
+        $file= $request->file('book_image');
+        $filename= date('YmdHi').$file->getClientOriginalName();
+        $file-> move(public_path('/images'), $filename);
+
+        $book->book_title = $data["book_title"];
+        $book->book_image = $filename;
+        $book->description = $data["description"];
+        $book->book_author = $data["book_author"];
+        $book->book_genre = $data["book_genre"];
+        $book->publisher_name = $data["publisher_name"];
+        $book->publication_date = $data["publication_date"];
+        $book->number_of_pages = $data["number_of_pages"];
+        $book->price = $data['price'];
+        $book->amazon_link = $data['amazon_link'];
+        $book->save();
+
+        return response()->json(["success" => true]);
+        
+    }
+
+
+    public function deleteBook(Request $request){
+        $book = Book::find($request['id']);
+        $book->delete();
+        return response()->json(["success" => true]);
+    }
 
 }

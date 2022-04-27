daad9f08c7ac8c340e7364c23a5cd323f37ab1c3
joe rizk <joerizk2@hotmail.com>
2022-04-27 18:43:42 +0300

    created book modal and migration

---
+++ storiad-laravel-challenge/database/migrations/2022_04_27_145740_create_books_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('book_title');
+            $table->binary('book_image');
+            $table->text('description');
+            $table->string('book_author');
+            $table->string('book_genre');
+            $table->string('publisher_name');
+            $table->date('publication_date');
+            $table->integer('number_of_pages');
+            $table->decimal('price',8,2);
+            $table->string('amazon_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};

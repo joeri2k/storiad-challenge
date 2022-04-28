7349b6853b3d70b7dcb3e413bfd0e0cd63a9a412
joe rizk <joerizk2@hotmail.com>
2022-04-28 12:29:47 +0300

    created header component

---
+++ storiad-frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div style={{ height: 75, backgroundColor: "#174554", color: "white" }}>
+      <div>BookShell</div>
+    </div>
+  );
+}
+
+export default Header;
